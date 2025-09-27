63e2e19bb031838d2702166af2797f6803b95830
Joval Gordon <jgordon95@hotmail.co.uk>
2025-09-27 16:03:28 +0100

    Basic task system

---
+++ Data/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuxDesk.Data
+{
+    public class UserTask
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Notes { get; set; } = string.Empty;
+        public DateOnly StartDate { get; set; }
+        public int Priority { get; set; } = 0;
+        public int Effort { get; set; } = 0;
+        public bool IsDone { get; set; }
+    }
+}
