583fd1bac0292537d70b8ff1560921b5557d3564
Joval Gordon <jgordon95@hotmail.co.uk>
2025-09-28 14:44:10 +0100

    Improved task class

--- Data/Task.cs
+++ Data/Task.cs
@@ -8,11 +8,18 @@ namespace AuxDesk.Data
 {
     public class UserTask
     {
+        public string TaskGUID { get; set; } = string.Empty;
+        public DateOnly AssignedDate { get; set; }
+        public DateTime? StartDateTime { get; set; } = null;
+        public DateTime? EndDateTime { get; set; } = null;
+        public int Priority { get; set; } = 0;
+        public bool IsDone { get; set; } = false;
+        public TaskData UserTaskData { get; set; } = new TaskData();
+    }
+    public class TaskData
+    {
+        public string TaskGUID { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
         public string Notes { get; set; } = string.Empty;
-        public DateOnly StartDate { get; set; }
-        public int Priority { get; set; } = 0;
-        public int Effort { get; set; } = 0;
-        public bool IsDone { get; set; }
     }
 }
