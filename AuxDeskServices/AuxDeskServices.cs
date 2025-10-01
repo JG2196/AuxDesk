f066ac0e56af2a7befe4533b45ac807d58f54a1c
Joval Gordon <jgordon95@hotmail.co.uk>
2025-10-01 17:21:27 +0100

    Backend shared services added

---
+++ AuxDeskServices/AuxDeskServices.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using AuxDesk.Data;
+
+namespace AuxDesk.AuxDeskServices
+{
+    public class AuxDeskServices
+    {
+        // FileSystem.AppDataDirectory - points to your app’s sandboxed data folder
+        private readonly string taskPath = Path.Combine(FileSystem.AppDataDirectory, "userTasks.json");
+
+        public List<UserTask> GetUserTasks()
+        {
+            List<UserTask> listUserTasks = new();
+
+            if (!File.Exists(taskPath)) { return null; }
+
+            var contents = File.ReadAllText(taskPath);
+            var savedItems = JsonSerializer.Deserialize<List<UserTask>>(contents);
+            listUserTasks.AddRange(savedItems);
+
+            return listUserTasks;
+        }
+        public async Task Save(List<UserTask> listUserTasks)
+        {
+            var contents = JsonSerializer.Serialize(listUserTasks);
+
+            File.WriteAllText(taskPath, contents);
+
+            Console.WriteLine("List Saved", $"List has been saved to {taskPath}");
+        }
+    }
+
+}
