6a627bd752e4e932d20daf99025213b8f72e50d5
Joval Gordon <jgordon95@hotmail.co.uk>
2025-11-11 23:57:25 +0000

    Removed original AuxDeskServices

--- AuxDeskServices/AuxDesk_CRUDOps.cs
+++ AuxDeskServices/AuxDesk_CRUDOps.cs
@@ -9,35 +9,35 @@ namespace AuxDesk.AuxDeskServices
 {
     public partial class AuxDeskServices
     {
-        public void CRUDPermenentDelete()
-        {
-            List<UserTask> listUserTasks = GetUserTasks();
+        //public void CRUDPermenentDelete()
+        //{
+        //    List<UserTask> listUserTasks = GetUserTasks();
 
-            DateOnly expireDate = DateOnly.FromDateTime(DateTime.UtcNow.Date.AddDays(-14));
+        //    DateOnly expireDate = DateOnly.FromDateTime(DateTime.UtcNow.Date.AddDays(-14));
 
-            listUserTasks = CRUDRemoveDeletedTasks(listUserTasks, expireDate);
+        //    listUserTasks = CRUDRemoveDeletedTasks(listUserTasks, expireDate);
             
-            var expireDateTime = expireDate.ToDateTime(TimeOnly.MaxValue);
-            listUserTasks = listUserTasks.Where(task => !task.IsDone || task.EndDateTime != null && task.EndDateTime >= expireDateTime).ToList();
+        //    var expireDateTime = expireDate.ToDateTime(TimeOnly.MaxValue);
+        //    listUserTasks = listUserTasks.Where(task => !task.IsDone || task.EndDateTime != null && task.EndDateTime >= expireDateTime).ToList();
 
-            Save(listUserTasks, null, false);
-        }
-        private List<UserTask> CRUDRemoveDeletedTasks(List<UserTask> listUserTasks, DateOnly expireDate)
-        {
-            List<DeletedTask> listDeletedTasks = GetDeletedTasks();
-            if (listDeletedTasks.Count == 0) { return listUserTasks; }
+        //    Save(listUserTasks, null, false);
+        //}
+        //private List<UserTask> CRUDRemoveDeletedTasks(List<UserTask> listUserTasks, DateOnly expireDate)
+        //{
+        //    List<DeletedTask> listDeletedTasks = GetDeletedTasks();
+        //    if (listDeletedTasks.Count == 0) { return listUserTasks; }
 
-            List<DeletedTask> listExpiredTasks = listDeletedTasks.Where(task => task.TaskDeletedDate < expireDate).ToList();
-            if (listExpiredTasks.Count == 0) { return listUserTasks; }
+        //    List<DeletedTask> listExpiredTasks = listDeletedTasks.Where(task => task.TaskDeletedDate < expireDate).ToList();
+        //    if (listExpiredTasks.Count == 0) { return listUserTasks; }
 
-            foreach (DeletedTask expiredTask in listExpiredTasks)
-            {
-                listUserTasks.RemoveAll(task => task.TaskGUID == expiredTask.TaskGUID);
-                listDeletedTasks.RemoveAll(task => task.TaskGUID == expiredTask.TaskGUID);
-            }
+        //    foreach (DeletedTask expiredTask in listExpiredTasks)
+        //    {
+        //        listUserTasks.RemoveAll(task => task.TaskGUID == expiredTask.TaskGUID);
+        //        listDeletedTasks.RemoveAll(task => task.TaskGUID == expiredTask.TaskGUID);
+        //    }
 
-            Save(null, listDeletedTasks, true);
-            return listUserTasks;
-        }
+        //    Save(null, listDeletedTasks, true);
+        //    return listUserTasks;
+        //}
     }
 }
