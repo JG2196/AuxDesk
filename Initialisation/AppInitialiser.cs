8de57355501c1380372f6dbce4b394b4ac41f1fc
Joval Gordon <jgordon95@hotmail.co.uk>
2025-11-19 15:13:35 +0000

    CRUD structure in place

---
+++ Initialisation/AppInitialiser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuxDesk.Initialisation
+{
+    using AuxDesk.Data;
+    using AuxDesk.Models;
+
+    internal class AppInitialiser
+    {
+        private readonly ITaskRepository _taskRepository;
+        private readonly IRecycleRepository _recycleRepository;
+
+        public AppInitialiser(ITaskRepository taskRepository, IRecycleRepository recycleRepository)
+        {
+            _taskRepository = taskRepository;
+            _recycleRepository = recycleRepository;
+        }
+
+        public async Task InitialiseAsync()
+        {
+            Console.WriteLine("Initializing application...");
+
+            // 1. Seed default data
+            //await SeedDefaultTasksAsync();
+
+            // 2. Clean up recycle bin
+            //await CleanupRecycleBinAsync();
+
+            // 3. Validate data integrity
+            //await ValidateDataAsync();
+
+            Console.WriteLine("Application initialized successfully!");
+        }
+    }
+}
