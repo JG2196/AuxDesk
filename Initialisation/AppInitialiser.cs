a4166deb6a6cfcb98a37b8e4f04f061912c94c09
Joval Gordon <jgordon95@hotmail.co.uk>
2026-01-02 16:49:34 +0000

    CRON init functions added

--- Initialisation/AppInitialiser.cs
+++ Initialisation/AppInitialiser.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace AuxDesk.Initialisation
 {
     using AuxDesk.Data;
-    using AuxDesk.Models;
+    //using AuxDesk.Models;
 
     internal class AppInitialiser
     {
@@ -24,11 +24,8 @@ namespace AuxDesk.Initialisation
         {
             try
             {
-                // 1. Check if files exist, if not create files
+                // Check if files exist, if not create files
                 await ValidateExistingFiles();
-
-                // 2. Clean up recycle bin
-                await CleanupRecycleBinAsync();
             }
             catch(Exception ex)
             {
@@ -46,43 +43,6 @@ namespace AuxDesk.Initialisation
             catch (Exception ex)
             {
                 Console.WriteLine($"ValidateExistingFiles ex: {ex.Message}");
-            }
-        }
-        private async Task CleanupRecycleBinAsync()
-        {
-            try
-            {
-                // Get deleted tasks
-                List<DeletedTaskItem> listRecycledTaskItems = await _recycleRepository.GetAllAsync();
-
-                // If empty return
-                if (listRecycledTaskItems.Count == 0) { return; }
-
-                // Else get all tasks
-                List<TaskItem> listTaskItems = await _taskRepository.GetAllAsync();
-
-                // Set expire date (14 days before current date)
-                DateOnly expireDate = DateOnly.FromDateTime(DateTime.UtcNow.Date.AddDays(-14));
-
-                // Filter deleted tasks to get expired tasks
-                List<DeletedTaskItem> listExpiredTasks = listRecycledTaskItems.Where(task => task.DateDeleted < expireDate).ToList();
-                if (listExpiredTasks.Count == 0) { return; }
-
-                // Remove expired tasks from all tasks
-                // Remove expired tasks from deleted tasks
-                foreach (DeletedTaskItem expiredTask in listExpiredTasks)
-                {
-                    listTaskItems.RemoveAll(task => task.TaskGUID == expiredTask.TaskGUID);
-                    listRecycledTaskItems.RemoveAll(task => task.TaskGUID == expiredTask.TaskGUID);
-                }
-
-                // Save files
-                await _taskRepository.SaveAsync(listTaskItems);
-                await _recycleRepository.SaveAsync(listRecycledTaskItems);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"CleanupRecycleBinAsync ex: {ex.Message}");
             }
         }
     }
