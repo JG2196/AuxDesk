4242009237a20c5a29cba518e258d5805f3b009f
Joval Gordon <jgordon95@hotmail.co.uk>
2026-01-10 16:08:45 +0000

    Organising timer features

---
+++ Services/ITimerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuxDesk.Services
+{
+    using AuxDesk.Models;
+
+    public interface ITimerService
+    {
+        Task<List<TaskItem>> GetTaskItemsAsync(DateOnly? selectedDate);
+    }
+}
