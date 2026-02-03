4c4bd590dc488b457d0d6b132a30c05836194480
Joval Gordon <jgordon95@hotmail.co.uk>
2026-02-03 18:48:56 +0000

    - Problem with timer integration, remove one of the timers whilst finding solution

--- Services/ProtoITimerService.cs
+++ Services/ProtoITimerService.cs
@@ -8,19 +8,36 @@ namespace AuxDesk.Services
 {
     public interface ProtoITimerService
     {
-        event Action? OnTimerTick;
-        event Action? OnTimerStateChanged;
+        event Action? Countdown_OnTimerTick;
+        event Action? Countdown_OnTimerStateChanged;
 
-        bool bCountdownIsRunning();
-        bool bPomodoroIsRunning();
-        bool bCountdownIsPaused();
-        int GetPresetTimer();
+        event Action? Pomodoro_OnTimerTick;
+        event Action? Pomodoro_OnTimerStateChanged;
+
+        bool Countdown_IsRunning();
+        bool Countdown_IsPaused();
+        int Countdown_GetPresetTimer();
+        void Countdown_StartTimer();
+        void Countdown_PauseTimer();
+        void Countdown_StopTimer();
+        //
+        bool Pomodoro_IsRunning();
+        bool Pomodoro_IsPaused();
+        int Pomodoro_GetPomodoroType();
+        int Pomodoro_GetSetCycles();
+        int Pomodoro_GetCompletedCycles();
+        void Pomodoro_StartTimer(int pomodoroCycles);
+        void Pomodoro_PauseTimer();
+        void Pomodoro_StopTimer();
+        void Pomodoro_ResetTimer();
+
+
+
+
         void SetTimer(int selectedTime);
         string GetFormattedTime();
         void DisplayNotification(string message);
-        void PauseTimer();
         void TimerCleanUp();
-        void StartTimer();
-        void StopTimer();
+
     }
 }
--- Services/ProtoTimerService.cs
+++ Services/ProtoTimerService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Toolkit.Uwp.Notifications;
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Toolkit.Uwp.Notifications;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,17 +10,26 @@ namespace AuxDesk.Services
 {
     public class ProtoTimerService : ProtoITimerService
     {
-        public event Action? OnTimerTick;
-        public event Action? OnTimerStateChanged;
+        public event Action? Countdown_OnTimerTick;
+        public event Action? Countdown_OnTimerStateChanged;
+
+        public event Action? Pomodoro_OnTimerTick;
+        public event Action? Pomodoro_OnTimerStateChanged;
 
         private System.Timers.Timer? _timer;
         private TimeSpan _remainingTime;
         private bool _countdownIsRunning;
         private bool _pomodoroIsRunning;
         private bool _countdownIsPaused = false;
+        private bool _pomodoroIsPaused = false;
+        private int _pomodoroType = 3;
         private int _presetTimer = 1;
 
-        public void StartTimer()
+        private int pomodoroCycles;
+        private int selectedCycles = 4;
+        bool bOnBreak = false;
+
+        public void Countdown_StartTimer()
         {
             if (_countdownIsRunning && _countdownIsPaused)
             {
@@ -29,26 +39,26 @@ namespace AuxDesk.Services
             else
             {
                 _timer = new System.Timers.Timer(1000); // 1 second interval
-                _timer.Elapsed += OnTimerElapsed;
+                _timer.Elapsed += Countdown_OnTimerElapsed;
                 _timer.AutoReset = true;
                 _timer.Start();
                 _countdownIsRunning = true;
             }
-            OnTimerStateChanged?.Invoke();
+            Countdown_OnTimerStateChanged?.Invoke();
         }
-        private void OnTimerElapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        private void Countdown_OnTimerElapsed(object? sender, System.Timers.ElapsedEventArgs e)
         {
             _remainingTime = _remainingTime.Subtract(TimeSpan.FromSeconds(1));
 
             if (_remainingTime.TotalSeconds <= 0)
             {
-                StopTimer();
+                Countdown_StopTimer();
                 DisplayNotification("Timer done");
             }
 
-            OnTimerTick?.Invoke();
+            Countdown_OnTimerTick?.Invoke();
         }
-        public void PauseTimer()
+        public void Countdown_PauseTimer()
         {
             if (!_countdownIsRunning)
             {
@@ -61,28 +71,26 @@ namespace AuxDesk.Services
                 _countdownIsPaused = true;
             }
         }
-        public void StopTimer()
+        public void Countdown_StopTimer()
         {
             _countdownIsRunning = false;
+            SetTimer(_presetTimer);
+            _countdownIsPaused = false;
 
             if (_timer != null)
             {
                 _timer.Stop();
-                _timer.Elapsed -= OnTimerElapsed;
+                _timer.Elapsed -= Countdown_OnTimerElapsed;
                 _timer.Dispose();
                 _timer = null;
             }
 
-            OnTimerStateChanged?.Invoke();
+            Countdown_OnTimerStateChanged?.Invoke();
         }
-        public bool bCountdownIsRunning() => _countdownIsRunning;
-        public bool bPomodoroIsRunning() => _pomodoroIsRunning;
-        public bool bCountdownIsPaused() => _countdownIsPaused;
-        public int GetPresetTimer() => _presetTimer;
-        public string GetFormattedTime()
-        {
-            return $"{_remainingTime.Hours:D2}:{_remainingTime.Minutes:D2}:{_remainingTime.Seconds:D2}";
-        }
+        public bool Countdown_IsRunning() => _countdownIsRunning;
+        public bool Countdown_IsPaused() => _countdownIsPaused;
+        public int Countdown_GetPresetTimer() => _presetTimer;
+        
         public void SetTimer(int selectedDuration)
         {
             _presetTimer = selectedDuration;
@@ -90,10 +98,131 @@ namespace AuxDesk.Services
             _remainingTime = selectedDuration switch
             {
                 0 => TimeSpan.FromMinutes(15),
-                1 => TimeSpan.FromMinutes(30),
+                //1 => TimeSpan.FromMinutes(30),
                 2 => TimeSpan.FromHours(1),
+                //3 => TimeSpan.FromMinutes(5),
+                //4 => TimeSpan.FromMinutes(25),
+                3 => TimeSpan.FromSeconds(3),
+                4 => TimeSpan.FromSeconds(3),
+                1 => TimeSpan.FromSeconds(3),
                 _ => TimeSpan.FromMinutes(30)
             };
+        }
+        //
+        public bool Pomodoro_IsRunning() => _pomodoroIsRunning;
+        public bool Pomodoro_IsPaused() => _pomodoroIsPaused;
+        public int Pomodoro_GetPomodoroType() => _pomodoroType;
+        public int Pomodoro_GetSetCycles() => selectedCycles;
+        public int Pomodoro_GetCompletedCycles() => pomodoroCycles;
+        public void Pomodoro_StartTimer(int pomodoroCycles)
+        {
+            if (_pomodoroIsRunning && _pomodoroIsPaused)
+            {
+                _timer.Start();
+                _pomodoroIsPaused = false;
+            }
+            else
+            {
+                _pomodoroType = 0;
+                selectedCycles = pomodoroCycles;
+                _timer = new System.Timers.Timer(1000); // 1 second interval
+                _timer.Elapsed += Pomodoro_OnTimerElapsed;
+                _timer.AutoReset = true;
+                _timer.Start();
+                _pomodoroIsRunning = true;
+            }
+            Pomodoro_OnTimerStateChanged?.Invoke();
+        }
+        private void Pomodoro_OnTimerElapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            _remainingTime = _remainingTime.Subtract(TimeSpan.FromSeconds(1));
+
+            string message = "";
+
+            if (pomodoroCycles >= selectedCycles)
+            {
+                _remainingTime = TimeSpan.Zero;
+                Pomodoro_StopTimer();
+                Pomodoro_ResetTimer();
+                DisplayNotification("Cycles completed");
+                _pomodoroType = 3;
+            }
+
+            else if (_remainingTime.TotalSeconds == 0 && bOnBreak)
+            {
+                bOnBreak = false;
+                SetTimer(4);
+                _pomodoroType = 0;
+                DisplayNotification("Break over! Time to work.");
+            }
+            else if (_remainingTime.TotalSeconds == 0 && !bOnBreak)
+            {
+                // Timer finished
+                pomodoroCycles++;
+                bOnBreak = true;
+                if (pomodoroCycles % 4 == 0 && pomodoroCycles != selectedCycles)
+                {
+                    SetTimer(1);
+                    _pomodoroType = 2;
+                    message = "Work session over! Time for a long break.";
+                }
+                else if (pomodoroCycles != selectedCycles)
+                {
+                    SetTimer(3);
+                    _pomodoroType = 1;
+                    message = "Work session over! Time for a short break.";
+                }
+                if (pomodoroCycles < selectedCycles)
+                {
+                    DisplayNotification(message);
+                }
+            
+            }
+            Pomodoro_OnTimerTick?.Invoke();
+        }
+        public void Pomodoro_PauseTimer()
+        {
+            if (!_pomodoroIsRunning)
+            {
+                return;
+            }
+
+            if (_timer != null)
+            {
+                _timer.Stop();
+                _pomodoroIsPaused = true;
+            }
+        }
+        public void Pomodoro_StopTimer()
+        {
+            _pomodoroIsRunning = false;
+
+            if (_timer != null)
+            {
+                pomodoroCycles = 0;
+                _timer.Stop();
+                _timer.Elapsed -= Pomodoro_OnTimerElapsed;
+                _timer.Dispose();
+                _timer = null;
+            }
+
+            Pomodoro_OnTimerStateChanged?.Invoke();
+        }
+        public void Pomodoro_ResetTimer()
+        {
+            _remainingTime = TimeSpan.Zero;
+            _pomodoroIsRunning = false;
+            _pomodoroIsPaused = false;
+            _pomodoroType = 0;
+            _presetTimer = 1;
+
+            pomodoroCycles = 0;
+            bOnBreak = false;
+        }
+        //
+        public string GetFormattedTime()
+        {
+            return $"{_remainingTime.Hours:D2}:{_remainingTime.Minutes:D2}:{_remainingTime.Seconds:D2}";
         }
         public void DisplayNotification(string message)
         {
